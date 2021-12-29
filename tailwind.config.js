494464b8ae34cfe0ef6f037beedff290b8605a3b
Blackdr3464 <63839741+Blackdr3464@users.noreply.github.com>
2021-12-29 16:39:23 +0700

    deploy

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: ['**/*.html'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#dc143c',
+                bg_color: '#0d0d0d',
+                gray_color: '#1a1a1a',
+                text_gray: '#4a4b54',
+            },
+            borderRadius: {
+                elip_egg: '17% 64% 42% 53% / 15% 30% 66% 76% ',
+            },
+            boxShadow: {
+                titi: '4px 6px 2px 0 rgb(0,0,0)',
+            },
+        },
+    },
+    plugins: [],
+};
